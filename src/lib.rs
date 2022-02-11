1a42957af2ecd9a86fe0babad7ec4a4b548f795b
prz23 <panruizheng@zju.edu.cn>
2022-02-11 16:37:36 +0800

    use rand::rngs::OsRng

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,8 @@ extern crate lazy_static;
 mod config;
 use config::{ZKError, Hash, HASH};
 use rand::Rng;
-use rand_core::OsRng;
+use rand::rngs::OsRng;
+// use rand_core::OsRng;
 use sha3::Sha3_512;
 use std::convert::TryInto;
 
